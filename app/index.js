3e01efe2d698c031be548412968195b1924a790e
Buck Golemon <buck@yelp.com>
2014-11-06 12:40:37 -0800

    able to regenerate yelp_uri pylintrc

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 'use strict';
-var util = require('util');
-var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
+var _ = require('lodash');
+
+var appendList = function(defaults) {
+  return function(input) {
+    return _.compact(defaults.concat(input.split(',').map(_.trim)));
+  };
+};
 
 var PyMicrolibGenerator = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../package.json');
-  },
-
   prompting: function () {
     var done = this.async();
 
@@ -18,37 +19,27 @@ var PyMicrolibGenerator = yeoman.generators.Base.extend({
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name: 'pylint.ignored-classes',
+      message: 'Which of your classes should pylint ignore?',
+      filter: appendList(['pytest']),
     }];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      this.props = props;
 
       done();
     }.bind(this));
   },
 
   writing: {
-    app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('app/templates');
-
-      this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
-    },
-
-    projectfiles: function () {
-      this.src.copy('editorconfig', '.editorconfig');
-      this.src.copy('jshintrc', '.jshintrc');
+    pylint: function() {
+      this.template(
+        'pylintrc',
+        'pylintrc',
+        { props: this.props }
+      );
     }
   },
-
-  end: function () {
-    this.installDependencies();
-  }
 });
 
 module.exports = PyMicrolibGenerator;
