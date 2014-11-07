e769c1b2a6cd9e3b7df991b40e0d66b62adb8e8a
Buck Golemon <buck@yelp.com>
2014-11-06 19:03:24 -0800

    add support for pylint globally-disabled rules

--- pylint/index.js
+++ pylint/index.js
@@ -1,45 +1,73 @@
 'use strict';
 var generators = require('yeoman-generator');
-var yosay = require('yosay');
 var _ = require('lodash');
 
-var appendList = function(defaults) {
-  return function(input) {
-    return _.compact(defaults.concat(input.split(',').map(_.trim)));
-  };
+var pylint = {
+  disabled: ['missing-docstring', 'locally-disabled'],
+  ignoredClasses: ['pytest'],
 };
 
-module.exports = generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
 
-    var key = 'pylint.ignored-classes';
-    var prompts = [{
-      name: key,
-      message: 'Which of your classes should pylint ignore?',
-      filter: appendList(['pytest']),
-      when: function(){
-        return this.config.get(key) == null;
-      }.bind(this)
-    }];
+var handleDefaults = function(props, defaults) {
+  defaults = defaults || pylint;
+  _.each(props, function (val, key) {
+    val = val || '';
+    val = val.split(',');
+    var defaultVal = defaults[key];
+    if (defaultVal === undefined) {
+      throw new Error('No default configured for "' + key + '"');
+    }
+    props[key] = _(defaultVal.concat(val))
+      .map(_.trim)
+      .compact()
+      .sort()
+      .uniq(true)
+      .value()
+      .join(',');
+  });
+  return props;
+};
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
 
-      done();
-    }.bind(this));
+module.exports = generators.Base.extend(
+  {
+    prompting: function () {
+      var done = this.async();
+
+      var prompts = [
+        {
+          name: 'ignoredClasses',
+          message: 'Which of your classes should pylint ignore?',
+          when: this.config.get('ignoredClasses') == null,
+        },
+        {
+          name: 'disabled',
+          message: 'Are there any pylint rules you need to disable?',
+          when: this.config.get('disabled') == null,
+        },
+      ];
+
+      this.prompt(prompts, function (props) {
+
+        this.props = {
+          pylint: handleDefaults(props)
+        };
+        done();
+      }.bind(this));
+    },
+
+    configuring: function() {
+      // http://yeoman.io/authoring/storage.html
+      this.config.set(this.props);
+    },
+
+    writing: function() {
+      this.fs.copyTpl(
+        this.templatePath('pylintrc'),
+        this.destinationPath('pylintrc'),
+        this.config.getAll()
+      );
+    },
   },
-
-  configuring: function() {
-    // http://yeoman.io/authoring/storage.html
-    this.config.set(this.props);
-  },
-
-  writing: function() {
-    this.fs.copyTpl(
-      this.templatePath('pylintrc'),
-      this.destinationPath('pylintrc'),
-      { props: this.config.getAll() }
-    );
-  }
-});
+  { handleDefaults: handleDefaults }
+);
--- test/test-pylint.js
+++ test/test-pylint.js
@@ -13,6 +13,38 @@ var fixtureFile = function(name) {
   fs.writeFileSync(name, fs.readFileSync(path.join(__dirname, 'fixtures', name)));
 };
 
+
+describe('py-microlib:pylint.handleDefaults', function () {
+  var pylint = require('../pylint');
+  it('handles undefined input', function(){
+    assert.deepEqual(
+        {a:'bar,foo'},
+        pylint.handleDefaults({a:undefined}, {
+          a:['foo', 'bar']
+        })
+    );
+  });
+  it('errors with undefined default', function(){
+    assert.throws(
+        function(){
+          pylint.handleDefaults({a:undefined}, {
+            g:['foo', 'bar']
+          });
+        },
+        Error
+    );
+  });
+  it('merges defined input', function(){
+    assert.deepEqual(
+        {a:'bar,cat,foo,zub'},
+        pylint.handleDefaults(
+          {a:'cat,zub'},
+          {a:['foo', 'bar']}
+        )
+    );
+  });
+});
+
 describe('py-microlib:pylint', function () {
   beforeEach(function () {
     this.run = helpers.run(path.join(__dirname, '../pylint'))
@@ -23,7 +55,7 @@ describe('py-microlib:pylint', function () {
   describe('without ignored classes', function(){
     beforeEach(function(done) {
       this.run
-        .withPrompt({'pylint.ignored-classes': []})
+        .withPrompt({'ignoredClasses': ''})
         .on('end', done);
     });
     it('creates files', function () {
@@ -35,11 +67,11 @@ describe('py-microlib:pylint', function () {
         'pylint',
         ['demo.py'],
         function(error, stdout, stderr){
-          assert.equal(error.code, 2);
           assert.equal(stderr, '');
           if (stdout.indexOf('Class \'MyClass\' has no \'x\' member') < 0) {
             throw Error(stdout);
           }
+          assert.equal(error.code, 2);
           done();
         }
       );
@@ -50,23 +82,23 @@ describe('py-microlib:pylint', function () {
   describe('with ignored classes', function(){
     beforeEach(function(done) {
       this.run
-        .withPrompt({'pylint.ignored-classes': ['MyClass']})
+        .withPrompt({'ignoredClasses': 'MyClass'})
         .on('end', done);
     });
     it('creates files', function () {
       assert.file(['pylintrc']);
     });
-    it('doesn\'t ignore MyClass', function(done) {
+    it('does ignore MyClass', function(done) {
       fixtureFile('demo.py');
       childProcess.execFile(
         'pylint',
         ['demo.py'],
         function(error, stdout, stderr){
-          assert.equal(error, null);
           assert.equal(stderr, '');
           if (stdout.indexOf('Class \'MyClass\' has no \'x\' member') >= 0) {
             throw Error(stdout);
           }
+          assert.equal(error, null);
           done();
         }
       );
