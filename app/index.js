a879ebe85adc97d114614e8b51d71fca09ff8e99
Simon Boudrias <admin@simonboudrias.com>
2014-11-06 17:17:09 -0800

    Add unit text config generation (tox/travis)

--- app/index.js
+++ app/index.js
@@ -5,6 +5,10 @@ var yosay = require('yosay');
 
 module.exports = generators.Base.extend({
   initializing: function () {
+      this.composeWith('py-microlib:tox', {}, {
+        local: path.join(__dirname, '../tox/index.js'),
+        link: 'strong'
+      });
       this.composeWith('py-microlib:pylint', {}, {
         local: path.join(__dirname, '../pylint/index.js'),
         link: 'strong'
